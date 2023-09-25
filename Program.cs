c5eed094880fda4d2841ff9254d65a5d2a319717
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-09-25 14:25:20 +0200

    Forts.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿namespace SUT_2023_09_25_Rep_Arrayer
+﻿using System.Net.NetworkInformation;
+
+namespace SUT_2023_09_25_Rep_Arrayer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //Hej
+            //Det var en gång två fåglar som ville bada. Ingen av dem hade dock badkläder!
+            //Hur tusan skulle de lösa detta? 
         }
     }
 }
