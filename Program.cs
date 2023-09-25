02fe17aaf8a5d0e4b74a4080bcf16cfdb6380239
Anna Söderberg <soderberganna@outlook.com>
2023-09-25 14:28:58 +0200

    Börjat på en berättelse om en tomte som möter en skogsmus i skogen

--- Program.cs
+++ Program.cs
@@ -11,6 +11,16 @@
 
         /*
          
+        Det var en gång en Tomte som bodde under en sten. Stenen var full med mossa och det tyckte tomten om.
+        Varje dag gick tomten ut för att plocka bär och nötter.
+        En dag när han var ute och gick träffade han på en skogsmus. 
+        Skogsmusen tittade förvånat på tomten däe ha gick med sin ryggsäck av näver 
+
+        - vad är du för en? frågade skogsmusen.
+        - Jag är tomten Tom, som är ute och letar nötter och bär. 
+        -Vem är du själv? frågade Tomten.
+
+
 
         */
 
