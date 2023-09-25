52eaf67433b6e339f1f3631ec2580eabbe2e4876
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-09-25 13:14:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SUT_2023_09_25_Rep_Arrayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
